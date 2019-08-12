a3c0148602d30e31d9635d70e89dc729348e454b
Koonail <330739264@qq.com>
2019-08-12 23:30:51 +0800

    注释变更

--- RemoveOutermostParentheses.java
+++ RemoveOutermostParentheses.java
@@ -30,10 +30,6 @@ package easy;
  * 解释：
  * 输入字符串为 "()()"，原语化分解得到 "()" + "()"，
  * 删除每个部分中的最外层括号后得到 "" + "" = ""。
- *
- * 来源：力扣（LeetCode）
- * 链接：https://leetcode-cn.com/problems/remove-outermost-parentheses
- * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  * @Author Koonail
  * @Date 2019/8/1 23:30
  **/
